a9b9ca4a276aa9a601eab292b39fd29d663b2ec3
fredricklou523 <fredrick.lou@gmail.com>
2018-10-03 14:24:21 -0700

    Created timerStates to track start, stop, pause functionality

---
+++ src/timerStates.js
@@ -0,0 +1,24 @@
+exports.timerState = {
+  NULL: 'NULL',
+  READY: 'READY',
+  RUNNING: 'RUNNING',
+  PAUSED: 'PAUSED',
+  FINISHED: 'FINISHED',
+  DISPOSED: 'DISPOSED'
+};
+
+exports.states = new set([
+  timerState.NULL,
+  timerState.READY,
+  timerState.RUNNING,
+  timerState.PAUSED,
+  timerState.FINISHED,
+  timerState.DISPOSED
+]);
+exports.startStates = new Set([
+  timerState.FINISHED,
+  timerState.READY,
+  timerState.PAUSED
+]);
+exports.stopStates = new Set([timerState.RUNNING, timerState.PAUSED]);
+exports.pauseStates = new Set([timerState.RUNNING]);
